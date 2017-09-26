7443c96f4e21069d55b07ff639bae51fb5a77762
idaherbertsson <32261365+idaherbertsson@users.noreply.github.com>
2017-09-26 15:29:44 +0200

    lagt till ny produkt

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ class Product:
 		self.price = price
 		self.count = count
 		self.tax = tax
+		
 
 	def price_incl_tax(self):
 		total=self.price * self.count * self.tax
@@ -14,7 +15,8 @@ class Product:
 
 
 products = [Product (price=900, count=2, tax= 1.25), 
-Product(price=100, count=1, tax=1.06), ]
+Product(price=100, count=1, tax=1.06), 
+Product (price = 470, count = 3, tax=1.12)]
 
 total_price = 0
 
@@ -22,7 +24,6 @@ for product in products:
 	total_price = total_price + product.price_incl_tax()
 
 print (total_price)
-
 
 
 
