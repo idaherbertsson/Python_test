622cc88eb4d45fbb5ebac3aee1a4b2060972a839
idaherbertsson <32261365+idaherbertsson@users.noreply.github.com>
2017-09-26 14:52:55 +0200

    Klass med konstruktor
    Kör med __init__ så. #6 i tutorial

--- test.py
+++ test.py
@@ -1,22 +1,21 @@
 class Product:
-	price = 0
-	count = 0
-	tax = 1.25
+	
+	def __init__(self, price, count, tax):
+		self.price = price
+		self.count = count
+		self.tax = tax
 
 	def price_incl_tax(self):
-		return(self.price * self.tax)
+		return (self.price * self.count * self.tax)
 
 
-robot = Product()
-robot.price = 900
-robot.count = 2
-
-book = Product()
-book.price = 100
-book.count = 1
-book.tax = 1.06
+robot= Product (price = 900, count = 2, tax = 1.25)
+book= Product (price = 100, count = 1, tax = 1.06)
 
 
-print(robot.price_incl_tax()*robot.count +book.price_incl_tax()*book.count)
+print(robot.price_incl_tax() + book.price_incl_tax())
+
+
+
 
 
